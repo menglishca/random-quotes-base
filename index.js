85fa84f9d7d5a84bfd6c43a731e31690bb349e64
Matthew English <matt.english4@gmail.com>
2024-10-03 11:15:30 -0230

    Improve the random quotes skeleton repo
    - Add more to the skeleton repo for random quotes
    - It was way too empty for an example, this should help make it easier to work with

--- index.js
+++ index.js
@@ -10,17 +10,36 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const quotes = [
+    { id: 1, text: "The only way to do great work is to love what you do.", date: new Date('2024-03-31') },
+    { id: 2, text: "Success is not final; failure is not fatal: It is the courage to continue that counts.", date: new Date() }
+];
+
 app.get('/', (request, response) => {
     response.redirect('/quotes');
 });
 
 app.post('/quotes', (request, response) => {
     const { quote } = request.body;
+    const newQuote = {
+        id: quotes.length + 1,
+        text: quote,
+        date: new Date()
+    };
+    quotes.push(newQuote);
+
     response.redirect('/quotes');
 });
 
 app.get('/quotes/:id', (request, response) => {
-    const { id } = request.params;
+    let { id } = request.params;
+    try {
+        id = parseInt(id, 10);
+    }
+    catch (exception) {
+        response.status(404).send("Quote not found");
+    }
+
     const quote = null;
     if (quote) {
         
